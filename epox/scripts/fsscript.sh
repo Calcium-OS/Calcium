c212c651330b3482a3e705f56589df63837f3e95
Ram10 <Rameezmajid10@outlook.com>
2026-07-07 16:40:56 +0100

    Update fsscript.sh

--- epox/scripts/fsscript.sh
+++ epox/scripts/fsscript.sh
@@ -382,6 +382,7 @@ run_optional "Gsettings Dock Change Part 1" gsettings set org.gnome.shell.extens
 run_optional "Gsettings Dock Change Part 2" gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
 run_optional "Gsettings Dock Change Part 3" gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
 run_optional "Set GNOME to dark mode" gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+run_optional "Automount Drives" gsettings set org.gnome.desktop.media-handling automount true
 
 echo ">>> Setting default wallpaper..."
 WALLPAPER_URL="https://images.steamusercontent.com/ugc/8546979052418597/251C5932F5CCC0355D748AA1A19608A0625C26E8/"
