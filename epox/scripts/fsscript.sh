8f6da3d5b4854b34b337c4bd6952be8f4b73f909
Internetperson-dev <88229217+Internetperson-dev@users.noreply.github.com>
2026-09-17 20:25:23 +0100

    Update fsscript.sh

--- epox/scripts/fsscript.sh
+++ epox/scripts/fsscript.sh
@@ -584,6 +584,7 @@ du -hx --max-depth=2 /usr /var /opt /home /root 2>/dev/null | sort -h -r | head 
 emerge -C www-client/epiphany
 run_optional "Remove outdated Image Viewer - Eye Of Gnome" emerge -C media-gfx/eog
 emerge -C gnome-extra/gnome-system-monitor
+emerge -C gnome-extra/nm-applet
 
 
 
